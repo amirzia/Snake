4ac8cc5f2c08150753e02a6ac6178eeaf4368e54
Hanieh Marvi <Hanieh.Marvi1@gmail.com>
2019-07-07 13:19:49 +0430

    Show both scores

--- snake.py
+++ snake.py
@@ -63,7 +63,7 @@ def game_over():
 # Score
 def show_score(choice, color, font, size):
     score_font = pygame.font.SysFont(font, size)
-    score_surface = score_font.render('Score : ' + str(score), True, color)
+    score_surface = score_font.render('Score : ' + str(scores[0]) + ' ' + str(scores[1]), True, color)
     score_rect = score_surface.get_rect()
     if choice == 1:
         score_rect.midtop = (frame_size_x/10, 15)
@@ -71,7 +71,7 @@ def show_score(choice, color, font, size):
         score_rect.midtop = (frame_size_x/2, frame_size_y/1.25)
     game_window.blit(score_surface, score_rect)
 
-score = 0
+scores = [0, 0, 0]
 game_board = []
 
 
@@ -83,6 +83,7 @@ while True:
     res = json.loads(res.text)
     game_board = res["board"]
     user_lost = res["lost"]
+    scores = res["scores"]
     if user_lost == True:
         print("end game")
         game_over()
