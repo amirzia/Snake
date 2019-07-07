6ab003b1ead9a3c209e40116e624e33363ef6ea2
Hanieh Marvi <Hanieh.Marvi1@gmail.com>
2019-07-07 13:10:32 +0430

    Add game over

--- snake.py
+++ snake.py
@@ -46,7 +46,30 @@ yellow = pygame.Color(255, 255, 0)
 
 # FPS (frames per second) controller
 fps_controller = pygame.time.Clock()
+def game_over():
+    my_font = pygame.font.SysFont('times new roman', 90)
+    game_over_surface = my_font.render('YOU DIED', True, red)
+    game_over_rect = game_over_surface.get_rect()
+    game_over_rect.midtop = (frame_size_x/2, frame_size_y/4)
+    game_window.fill(black)
+    game_window.blit(game_over_surface, game_over_rect)
+    show_score(0, red, 'times', 20)
+    pygame.display.flip()
+    time.sleep(3)
+    pygame.quit()
+    sys.exit()
 
+
+# Score
+def show_score(choice, color, font, size):
+    score_font = pygame.font.SysFont(font, size)
+    score_surface = score_font.render('Score : ' + str(score), True, color)
+    score_rect = score_surface.get_rect()
+    if choice == 1:
+        score_rect.midtop = (frame_size_x/10, 15)
+    else:
+        score_rect.midtop = (frame_size_x/2, frame_size_y/1.25)
+    game_window.blit(score_surface, score_rect)
 
 score = 0
 game_board = []
@@ -62,6 +85,7 @@ while True:
     user_lost = res["lost"]
     if user_lost == True:
         print("end game")
+        game_over()
     # print(len(game_board))
     # print(game_board)
     game_window.fill(black)
